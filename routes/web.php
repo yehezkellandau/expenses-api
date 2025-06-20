829aedfafada5edde53d91eadb041b983ad8b75e
yeheland <yeland_97@yhaoo.fr>
2025-06-20 15:21:55 -0400

    back to normal for auto deploy

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version(),
-"test" => "runner"];
+    return ['Laravel' => app()->version();
 });
