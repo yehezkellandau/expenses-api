7a8be4a25c6c93943f7a308a14100c21577b6403
yeheland <yeland_97@yhaoo.fr>
2025-06-20 14:15:29 -0400

    back to testing mode API

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version(),
-"test" => "uptodate"];
+    return ['Laravel' => app()->version()];
 });
