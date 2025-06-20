7d6f5bf40f2b3ad02eab347524cd366f09e2cd65
yeheland <yeland_97@yhaoo.fr>
2025-06-20 15:20:38 -0400

    test runner

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Laravel' => app()->version(),
+"test" => "runner"];
 });
