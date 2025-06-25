97fb506e3c14927db0ac6d0432cb074424402087
yeheland <yeland_97@yhaoo.fr>
2025-06-25 14:22:03 -0400

    cors fix 6

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173'],
     'allowed_origins_patterns' => [],
